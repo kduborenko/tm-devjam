6064bec9aad9ab548e20a17fe322c42be0c05cd1
Kiryl Dubarenka <k.duborenko@gmail.com>
2016-08-25 09:46:17 -0700

    Dummy page

--- src/main/java/com/epam/tmdevjam/web/Example.java
+++ src/main/java/com/epam/tmdevjam/web/Example.java
@@ -1,17 +1,20 @@
 package com.epam.tmdevjam.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Kiryl Dubarenka
  */
-@RestController
+@Controller
+@RequestMapping("/")
 public class Example {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    @GetMapping
+    public ModelAndView list() {
+        return new ModelAndView("index");
     }
 
 }
