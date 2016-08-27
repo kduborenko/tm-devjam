726d3aea0604ac7bb29e7a9eebd0216e604dfd36
Kiryl Dubarenka <k.duborenko@gmail.com>
2016-08-27 11:02:47 -0700

    Commit #n

--- src/main/java/com/epam/tmdevjam/web/Discovery.java
+++ src/main/java/com/epam/tmdevjam/web/Discovery.java
@@ -3,8 +3,11 @@ package com.epam.tmdevjam.web;
 import com.epam.tmdevjam.api.DiscoveryFacade;
 import com.ticketmaster.api.discovery.operation.SearchEventsOperation;
 import com.ticketmaster.api.discovery.response.PagedResponse;
+import com.ticketmaster.discovery.model.Date;
+import com.ticketmaster.discovery.model.Event;
 import com.ticketmaster.discovery.model.Events;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 /**
@@ -23,12 +29,40 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/discovery")
 public class Discovery {
+
     @Autowired
     private DiscoveryFacade discoveryFacade;
+
+    @Value("${ticketMaster.mainCategory}")
+    private String category;
+
     @RequestMapping(method = RequestMethod.GET)
     public PagedResponse<Events> list(@RequestParam(required = false) String keyword) throws IOException {
         SearchEventsOperation operation = new SearchEventsOperation();
         operation.keyword(keyword);
         return discoveryFacade.searchEvents(operation);
+    }
+
+    @RequestMapping(value = "timeline", method = RequestMethod.GET)
+    public Map<Date, List<Event>> timeline(@RequestParam(required = false) String keyword) throws IOException {
+        SearchEventsOperation operation = new SearchEventsOperation();
+        operation.keyword(keyword);
+        PagedResponse<Events> list = discoveryFacade.searchEvents(operation);
+
+        Map<Date, List<Event>> byDate = list.getContent()
+                .getEvents()
+                .stream()
+                .filter(this::checkClassification)
+                .collect(Collectors.groupingBy((event) -> event.getDates()));
+
+        return byDate;
+    }
+
+    private boolean checkClassification(Event e) {
+        return e.getClassifications()
+                .stream()
+                .filter(c -> this.category.equals(c.getSegment().getId()))
+                .findAny()
+                .isPresent();
     }
 }
