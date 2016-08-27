e5375507f9ffb5bd342e262535c5a02fb522e519
Yaroslav Karandashev <yarix7@gmail.com>
2016-08-27 14:43:13 -0700

    http requests

--- src/main/resources/static/TicketsPathApp.js
+++ src/main/resources/static/TicketsPathApp.js
@@ -1,14 +1,44 @@
 var TicketPathApp = angular.module('TicketPathApp', ['ngMaterial']);
 
 TicketPathApp
-    .controller('TicketCtrl', function($scope) {
-        $scope.message = 'Message scope!';
-        $scope.buttonTitle = 'Goooo!';
+    .controller('TicketCtrl', function($scope, $http) {
+        console.log('TicketCtrl', $scope, $http);
+
+        $http.get('/artists').then(function(response) {
+            console.log('artists', response.data);
+            $scope.groups = response.data;
+        });
+
 
         $scope.group = {};
         $scope.group.groupTitle = 'AC/DC';
         $scope.group.events = [
-            { title: 'One concert' },
-            { title: 'Another concert' }
+            { title: 'One concert', selected: false },
+            { title: 'Another concert', selected: true },
+            { title: 'Another 1', selected: false }
         ];
+
+        $scope.selectedItem = '';
+
+        $scope.querySearch = function() {
+            return $scope.groups;
+        };
+
+        $scope.searchTextChange = function() {
+            return $scope.groups;
+        };
+
+        $scope.selectedItemChange = function(item) {
+            $scope.selectedItem = item;
+            $http.get('/artists/' + item.id).then(function(response) {
+                console.log('event', response);
+
+            });
+        };
+
+        $scope.selectEvent = function(event) {
+            console.log('selectEvent', event)
+            $scope.group.events.forEach(function(item) { item.selected = false});
+            event.selected = true;
+        };
     });
